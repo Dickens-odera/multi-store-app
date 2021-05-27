329b834dd559cf291b3c34e61196bcf5c6c1f1f0
Dickens-Odera <dickensodera9@gmail.com>
2021-05-27 08:14:36 +0300

    Added admin store management backend

---
+++ database/migrations/2021_05_27_042542_create_stores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->index();
+            $table->enum('status',['active','deactivated'])->default('active');
+            $table->text('description')->nullable();
+            $table->softDeletesTz();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
