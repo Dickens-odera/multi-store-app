2fd6c627b8de6bbf8293bba194355d708da38bbd
Dickens-Odera <dickensodera9@gmail.com>
2021-05-28 12:58:44 +0300

    Added product purchases table

---
+++ database/migrations/2021_05_28_095031_create_product_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductPurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->foreignId('product_id')->index();
+            $table->integer('qty');
+            $table->decimal('total',8,2);
+            $table->softDeletesTz();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_purchases');
+    }
+}
